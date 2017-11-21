4c965e90934cf3942c6791ba8307fd74160d9662
oneia <studenticx@gmail.com>
2017-11-21 20:32:57 +0200

    - init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  template: `
+    <h1>Hello Sasha</h1>
+  `,
+  styleUrls: []
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent { }
